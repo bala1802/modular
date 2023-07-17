bc8913293dd3d67a8b755eff3b9899418e01ca16
Balaguru Sivasambagupta <bala1802@live.com>
2023-07-17 20:42:35 +0530

    added scheduler.step and modified params

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ def train(model, device, train_loader, optimizer, scheduler, epoch, loss_functio
     correct = 0
     processed = 0
 
+    losses = []
+
     for batch_idx, (data, target) in enumerate(pbar):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -17,6 +19,7 @@ def train(model, device, train_loader, optimizer, scheduler, epoch, loss_functio
 
         # loss = F.nll_loss(y_pred, target)
         loss = loss_function(y_pred, target)
+        losses.append(loss.item())
 
         loss.backward()
         optimizer.step()
@@ -28,3 +31,5 @@ def train(model, device, train_loader, optimizer, scheduler, epoch, loss_functio
         processed += len(data)
 
         pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+    
+    scheduler.step(sum(losses)/len(losses))

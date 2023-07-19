c8850f73b5630f0eae35f399bb7f67175990f8c1
Balaguru Sivasambagupta <bala1802@live.com>
2023-07-19 16:34:01 +0530

    added pct param

--- modelutils.py
+++ modelutils.py
@@ -50,11 +50,12 @@ def construct_scheduler(optimizer, data_loader, epochs, maximum_learning_rate):
         max_lr=maximum_learning_rate,
         steps_per_epoch=len(data_loader),
         epochs=epochs,
-        pct_start=5/epochs,
+        pct_start=params["one_cycle_lr_pct_start"]/epochs,
         div_factor=params["one_cycle_lr_div_factor"],
         three_phase=False,
         final_div_factor=params["one_cycle_lr_final_div_factor"],
         anneal_strategy=params["one_cycle_lr_anneal_strategy"],
-        verbose=params["one_cycle_lr_verbose"]
+        verbose=params["one_cycle_lr_verbose"],
+        
     )
     return scheduler
